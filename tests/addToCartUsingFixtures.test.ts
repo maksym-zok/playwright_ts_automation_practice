34e64215b11c07a08eb8d41877de8a2fb5a6a1d0
maksym-zok <81558365+maksym-zok@users.noreply.github.com>
2024-02-26 20:01:36 +0200

    Update addToCartUsingFixtures.test.ts

--- tests/addToCartUsingFixtures.test.ts
+++ tests/addToCartUsingFixtures.test.ts
@@ -1,19 +1,19 @@
 import { expect, test, } from '../base/pomFixture';
 import * as data from "../test-data/addToCart-test-data.json"
 
-test("Register test_01", async({page, baseURL, registerPage}) => {
-    await page.goto(`${baseURL}route=account/register`)
-    await registerPage.enterFirstName(data.firstName)
-    await registerPage.enterLastName(data.secondName)
-    await registerPage.enterEmail(data.email)
-    await registerPage.enterPhoneNumber(data.phoneNumber)
-    await registerPage.enterPassword(data.password)
-    await registerPage.enterConfirmPassword(data.password)
-    await registerPage.isSubscribeChecked()
-    await registerPage.clickTermsCondition()
-    await registerPage.clickToContinue()
-    expect(await page.title()).toBe("Your Account Has Been Created!")
-})
+// test("Register test_01", async({page, baseURL, registerPage}) => {
+//     await page.goto(`${baseURL}route=account/register`)
+//     await registerPage.enterFirstName(data.firstName)
+//     await registerPage.enterLastName(data.secondName)
+//     await registerPage.enterEmail(data.email)
+//     await registerPage.enterPhoneNumber(data.phoneNumber)
+//     await registerPage.enterPassword(data.password)
+//     await registerPage.enterConfirmPassword(data.password)
+//     await registerPage.isSubscribeChecked()
+//     await registerPage.clickTermsCondition()
+//     await registerPage.clickToContinue()
+//     expect(await page.title()).toBe("Your Account Has Been Created!")
+// })
 
 test("Login test_01", async({page, baseURL, loginPage}) => {
     await page.goto(`${baseURL}route=account/login`)
